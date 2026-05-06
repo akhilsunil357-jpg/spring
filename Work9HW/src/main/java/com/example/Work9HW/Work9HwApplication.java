ac757291831a0f6de2761d060fed8645b28b3443
Akhil sunil <akhilsunil357@gmail.com>
2026-05-06 12:59:49 +0530

    Work9HW

---
+++ Work9HW/src/main/java/com/example/Work9HW/Work9HwApplication.java
@@ -0,0 +1,13 @@
+package com.example.Work9HW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Work9HwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Work9HwApplication.class, args);
+	}
+
+}
